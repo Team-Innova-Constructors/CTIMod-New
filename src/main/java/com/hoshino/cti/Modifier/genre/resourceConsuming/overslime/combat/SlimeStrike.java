007a553a6f087bd5b68b0113989340ef3050807f
EtSH-C2H6S <2130679254@qq.com>
2026-01-19 20:11:26 +0800

    补充了一些提交

---
+++ src/main/java/com/hoshino/cti/Modifier/genre/resourceConsuming/overslime/combat/SlimeStrike.java
@@ -0,0 +1,7 @@
+package com.hoshino.cti.Modifier.genre.resourceConsuming.overslime.combat;
+
+import com.hoshino.cti.Modifier.genre.resourceConsuming.overslime.base.BasicOverslimeModifier;
+
+public class SlimeStrike extends BasicOverslimeModifier {
+
+}
---
+++ src/main/java/com/hoshino/cti/content/materialGenre/GenreManager.java
@@ -0,0 +1,12 @@
+package com.hoshino.cti.content.materialGenre;
+
+import java.util.Map;
+
+public class GenreManager {
+    public static final int FE_VALUE = 1;
+    public static final int COMPRESSED_AIR_VALUE = 100;
+    public static final int OVERSLIME_VALUE = 10000;
+    public static final int DURABILITY_VALUE = 10000;
+
+    public static final MaterialGenre.ResourceConsumingGenre OVERSLIME_GENRE = new MaterialGenre.ResourceConsumingGenre("overslime");
+}

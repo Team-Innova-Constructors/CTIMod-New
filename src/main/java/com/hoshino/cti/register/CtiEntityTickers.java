59ab1bd9e148742cd5b8e5e8dcda90b6521332da
EtSH-C2H6S <2130679254@qq.com>
2026-08-10 10:57:40 +0800

    重做EntityTicker系统，现在有个每个玩家独立的表，便于后面做元素异常伤害并增强多人模式下的延迟伤害表现。现在ticker也采用MobEffectCategory便于后面判定正面负面。

--- src/main/java/com/hoshino/cti/register/CtiEntityTickers.java
+++ src/main/java/com/hoshino/cti/register/CtiEntityTickers.java
@@ -5,6 +5,7 @@ import com.hoshino.cti.content.entityTicker.tickers.EnderStiction;
 import com.hoshino.cti.content.entityTicker.EntityTicker;
 import com.hoshino.cti.content.entityTicker.tickers.*;
 import com.hoshino.cti.content.registry.CtiRegistry;
+import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -14,7 +15,7 @@ public class CtiEntityTickers {
     public static final RegistryObject<EntityTicker> ORACLE = ENTITY_TICKERS.register("oracle", Oracle::new);
     public static final RegistryObject<EntityTicker> EMP = ENTITY_TICKERS.register("emp", Emp::new);
     public static final RegistryObject<EntityTicker> INVULNERABLE = ENTITY_TICKERS.register("invulnerable", InvulnerableTicker::new);
-    public static final RegistryObject<EntityTicker> SACRIFICE_SEAL = ENTITY_TICKERS.register("sacrifice_seal", EmptyTicker::new);
+    public static final RegistryObject<EntityTicker> SACRIFICE_SEAL = ENTITY_TICKERS.register("sacrifice_seal",()-> new EmptyTicker(MobEffectCategory.HARMFUL));
     public static final RegistryObject<EntityTicker> FIERY = ENTITY_TICKERS.register("fiery", Fiery::new);
     public static final RegistryObject<EntityTicker> VULNERABLE = ENTITY_TICKERS.register("vulnerable", VulnerableTicker::new);
     public static final RegistryObject<EntityTicker> SOUL = ENTITY_TICKERS.register("soul", Soul::new);

9e885ffe0718ca279128f239ee07226513b2e345
EtSH-C2H6S <2130679254@qq.com>
2026-09-16 22:20:32 +0800

    修复4k创电无掉落物

--- src/main/java/com/hoshino/cti/Blocks/CreativeEnergyCell4kBlock.java
+++ src/main/java/com/hoshino/cti/Blocks/CreativeEnergyCell4kBlock.java
@@ -5,9 +5,13 @@ import appeng.helpers.AEMaterials;
 import com.hoshino.cti.Blocks.BlockEntity.ae2.CreativeEnergyCell4kBE;
 import com.hoshino.cti.register.CtiBlockEntityType;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.loot.LootContext;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class CreativeEnergyCell4kBlock extends AEBaseEntityBlock<CreativeEnergyCell4kBE> {
     public CreativeEnergyCell4kBlock() {
@@ -17,5 +21,10 @@ public class CreativeEnergyCell4kBlock extends AEBaseEntityBlock<CreativeEnergyC
     @Override
     public @Nullable BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
         return new CreativeEnergyCell4kBE(pPos,pState);
+    }
+
+    @Override
+    public List<ItemStack> getDrops(BlockState pState, LootContext.Builder pBuilder) {
+        return List.of(new ItemStack(this.asItem()));
     }
 }
